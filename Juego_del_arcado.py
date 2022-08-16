099215e1ff479398efc96a9654cfce6165fc87c4
jeancar2001 <jean30001012001@gmail.com>
2022-08-16 17:44:20 -0500

    actualizacion del juego del aorcado

--- Juego_del_arcado.py
+++ Juego_del_arcado.py
@@ -4,7 +4,7 @@ import random
 def archivo():
     with open("./archivos/data.txt","r",encoding="utf-8") as f:
         word = [i for i in f]
-    return random.choice(word)
+    return random.choice(word).replace("\n","")
 
 def main():
     word = archivo()
@@ -13,8 +13,20 @@ def main():
     while True:
         print(word)
         a = {i:{b:"___ "} for i,b in enumerate(word)}
-        b = [b for i in a.values() for b in i.values()]
-        print("".join(b))
+        while True:
+            b = [b for i in a.values() for b in i.values()]
+            print("".join(b))
+            print("\n")
+            try:
+                word_user = input("Ingrese una letra: ")
+                if word_user.isnumeric():
+                    raise Exception ("Coloque solo letras en el espacio")
+            except Exception as f:
+                print(f)
+                continue
+            pass
+            break
         break
+        
 if __name__=="__main__":
     main()
