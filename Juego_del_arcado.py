a27f3f360cec982f7d87516b33be61e0d0c2e05b
jeancar2001 <jean30001012001@gmail.com>
2022-08-16 14:05:10 -0500

    juego aorcado actualizacion

---
+++ Juego_del_arcado.py
@@ -0,0 +1,21 @@
+from ntpath import join
+import os
+import random
+
+def archivo():
+    with open("./archivos/data.txt","r",encoding="utf-8") as f:
+        word = [i for i in f]
+    return random.choice(word)
+
+def main():
+    word = archivo()
+    os.system("clear")
+    print("\n\n")
+    while True:
+        print(word)
+        a = {i:{b:"___ "} for i,b in enumerate(word)}
+        b = [b for i in a.values() for b in i.values()]
+        print("".join(b))
+        break
+if __name__=="__main__":
+    main()
