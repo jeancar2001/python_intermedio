2cba85c8a46d243834e9e11b3d8f2818696c0f80
isauro2018 <darjony2015@gmail.com>
2022-08-18 03:01:32 -0500

    aorcado completado

--- Juego_del_arcado.py
+++ Juego_del_arcado.py
@@ -1,4 +1,3 @@
-from dbm import error
 import os
 import random
 
@@ -7,31 +6,46 @@ def archivo():
         word = [i for i in f]
     return random.choice(word).replace("\n","")
 
+def tildes(word,numer1,numer2):
+    a = [("á","a"),("é","e"),("í","i"),("ó","o"),("ú","u")]
+    for i in a:
+        word = word.replace(i[numer1],i[numer2])
+    return word
+
+def reemplazo(lista,word,dict):
+    for i in lista:
+        dict[i]=word[i].upper()+" "
+    return dict
+
+
+
 def main():
     word = archivo()
-    a = {i:{b.upper():"___ "} for i,b in enumerate(word)}
+    word_tilde = tildes(word,1,0)
+    word_sin_tilde = tildes(word,0,1)
+    a = {i[0]:"___ " for i in enumerate(word)}
     os.system("clear")
     while True:
-        print(word)
-        print(a)
         print("\n\n")
-        b = [b for i in a.values() for b in i.values()]
-        print("".join(b))
+        print("".join([i for i in a.values()]))
         print("\n")
+        t = list(filter(lambda i:i=="___ ",a.values()))
+        if len(t)==0:
+            break
         try:
             word_user = input("Ingrese una letra: ")
-            if word_user.isnumeric():
+            if word_user.isnumeric() or len(word_user)==0:
                 raise Exception ("Coloque solo letras en el espacio")
         except Exception as f:
             os.system("clear")
             print(f)
             continue
-        z = [i for i,p in enumerate(word) if p == word_user ]
+        z = [i[0] for i in enumerate(word_sin_tilde) if i[1]==word_user]+[i[0] for i in enumerate(word_tilde) if i[1]==word_user]
         if len(z)==0:
             os.system("clear")
             continue
-        print(z)
-        break
+        a = reemplazo(z,word,a)
+        os.system("clear")
         
         
 if __name__=="__main__":
