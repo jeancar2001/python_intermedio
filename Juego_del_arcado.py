81d472a67ad7e91e857b42b83cda6c6855cc9bcb
isauro2018 <darjony2015@gmail.com>
2022-08-21 18:32:40 -0500

    actualizacion de aorcado y acsi

--- Juego_del_arcado.py
+++ Juego_del_arcado.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 import os
 import random
 
@@ -23,19 +24,38 @@ def main():
     word = archivo()
     word_tilde = tildes(word,1,0)
     word_sin_tilde = tildes(word,0,1)
+    word_user_list = []
     a = {i[0]:"___ " for i in enumerate(word)}
     os.system("clear")
     print("Bienvenido al Juego de ahorcado")
+    vida = 6
+    puntuacion = 0
     while True:
+        print("\n")
+        print(f"VIDAS: {vida}")
         print("\n\n")
         print("".join([i for i in a.values()]))
         print("\n")
         t = list(filter(lambda i:i=="___ ",a.values()))
         if len(t)==0:
+            print(f"""
+Ganaste
+Puntuacion: {puntuacion}
+            """)
+            
+            break
+        elif vida== 0:
+            if puntuacion<0:
+                puntuacion = 0
+            print(f"""
+Se acabaron tus vidas fin del juego
+La palabra es: {word}
+Puntuacion: {puntuacion}
+                """)
             break
         try:
             word_user = input("Ingrese una letra: ")
-            if word_user.isnumeric() or len(word_user)==0:
+            if word_user.isnumeric() or len(word_user.replace("\t","").replace(" ",""))==0:
                 raise Exception ("Coloque solo letras en el espacio")
             if len(word_user)>1:
                 raise Exception ("Debe colocar una sola letra")
@@ -43,11 +63,21 @@ def main():
             os.system("clear")
             print(f)
             continue
+        if len(list(filter(lambda x: x==word_user ,word_user_list))) ==1:
+            os.system("clear")
+            print("Ingrese otra letra no repetida")
+            continue
+        word_user_list.append(word_user)
         z = [i[0] for i in enumerate(word_sin_tilde) if i[1]==word_user]+[i[0] for i in enumerate(word_tilde) if i[1]==word_user]
         if len(z)==0:
             os.system("clear")
+            vida -= 1
+            if vida>0:
+                print("PIERDES UNA VIDA")
+            puntuacion -= 10
             continue
         a = reemplazo(z,word,a)
+        puntuacion += 20
         os.system("clear")
         
         
--- dibujos_asci.py
+++ dibujos_asci.py
@@ -1,9 +1,46 @@
-print("""
-        bbbbbbbbb   b  bbbbbb b     b b           b bbbbbb b     b  b  bbb        bbb
-        b        b  b  b      bb    b  b         b  b      bb    b  b  b   b    b     b
-        b       b   b  b      b b   b   b       b   b      b b   b  b  b    b  b       b
-        bbbbbbbb    b  bbbbbb b  b  b    b     b    bbbbbb b  b  b  b  b    b  b       b
-        b       b   b  b      b   b b     b   b     b      b   b b  b  b    b  b       b
-        b        b  b  b      b    bb      b b      b      b    bb  b  b   b    b     b 
-        bbbbbbbbb   b  bbbbbb b     b       b       bbbbbb b     b  b  bbb        bbb
-        """)
+#print("""
+#        bbbbbbbbb   b  bbbbbb b     b b           b bbbbbb b     b  b  bbb        bbb
+#        b        b  b  b      bb    b  b         b  b      bb    b  b  b   b    b     b
+#        b       b   b  b      b b   b   b       b   b      b b   b  b  b    b  b       b
+#        bbbbbbbb    b  bbbbbb b  b  b    b     b    bbbbbb b  b  b  b  b    b  b       b
+#        b       b   b  b      b   b b     b   b     b      b   b b  b  b    b  b       b
+#        b        b  b  b      b    bb      b b      b      b    bb  b  b   b    b     b 
+#        bbbbbbbbb   b  bbbbbb b     b       b       bbbbbb b     b  b  bbb        bbb
+#        """)
+
+
+a = 6
+if a ==6:
+        print("""
+        O
+      / I \\
+      /  \\
+""")
+elif a==5:
+        print("""
+        O
+      / I \\
+      /  
+""")
+elif a==4:
+        print("""
+        O
+      / I \\
+      
+""")
+elif a ==3:
+        print("""
+        O
+      / I 
+""")
+elif a==2:
+        print("""
+        O
+        I 
+""")
+else a==1:
+        print("""
+        O
+
+""")
+
