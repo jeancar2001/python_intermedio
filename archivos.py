6c9c0ccefcb1f0882b76166c9292cf738a1bf71a
jeancar2001 <jean30001012001@gmail.com>
2022-09-09 19:25:53 -0500

    ultima actualizacion lenovo

--- archivos.py
+++ archivos.py
@@ -1,10 +1,7 @@
 
 def main():
-  a = []
   with open('./archivos/data.txt','r',encoding="utf-8") as b:
-    for i in b:
-      i = i.replace("\n","")
-      a.append(i)
+    a = b.read().replace("\n","-")
   
   print(a)
     
